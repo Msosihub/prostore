8110c457f1752b780c178bcf54ef86c3947636fb
Msosihub <msosihub@gmail.com>
2025-04-03 00:36:03 +0300

    Initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+import ProductList from "@/components/shared/product/product-list";
+import { getLatestProducts } from "@/lib/actions/product.actions";
+
+export const metadata = {
+  title: "Home",
+};
+
+export default async function Home() {
+  const latestProducts = await getLatestProducts();
+  return (
+    <>
+      <ProductList data={latestProducts} title="Newest Arrivals" limit={8} />
+    </>
+  );
+}
