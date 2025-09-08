1f3714e6d55f549aa017434bfa0f3f6c816083c8
Msosihub <msosihub@gmail.com>
2025-09-08 06:04:51 +0300

    fix_anysms

--- app/api/auth/send-sms/route.ts
+++ app/api/auth/send-sms/route.ts
@@ -1,24 +1,22 @@
-// import { NextResponse } from "next/server";
+import { sendSms } from "@/lib/africasTalking";
+import { NextResponse } from "next/server";
 
-// export async function POST(req: Request) {
-//   try {
-//     const { to, message } = await req.json();
+export async function POST(req: Request) {
+  try {
+    const { to, message } = await req.json();
 
-//     if (!to || !message) {
-//       return NextResponse.json(
-//         { error: "Missing to or message" },
-//         { status: 400 }
-//       );
-//     }
+    if (!to || !message) {
+      return NextResponse.json(
+        { error: "Missing to or message" },
+        { status: 400 }
+      );
+    }
 
-//     const response = await sendSms(to, message);
+    const response = await sendSms(to, message);
 
-//     return NextResponse.json({ success: true, response });
-//   } catch (error: any) {
-//     console.error("SMS error:", error);
-//     return NextResponse.json(
-//       { success: false, error: error.message },
-//       { status: 500 }
-//     );
-//   }
-// }
+    return NextResponse.json({ success: true, response });
+  } catch (error) {
+    console.error("SMS error:", error);
+    return NextResponse.json({ success: false, error: error }, { status: 500 });
+  }
+}
