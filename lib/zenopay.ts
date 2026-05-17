95b4dbbd695e263fafd90d73656cc742643158c1
Apple <bmcontractorstz@gmail.com>
2026-05-17 18:22:59 +0300

    Zeno api key log

--- lib/zenopay.ts
+++ lib/zenopay.ts
@@ -3,11 +3,16 @@ import { ZenoBody } from "@/types";
 const ZENOPAY_BASE_URL = "https://zenoapi.com";
 
 export async function zenopayRequest(endpoint: string, body: ZenoBody) {
+  if (!process.env.ZENOPAY_API_KEY) {
+    throw new Error("Missing ZENOPAY_API_KEY environment variable");
+  }
+  console.log("ZENO API KEY: ", process.env.ZENOPAY_API_KEY);
+
   const response = await fetch(`${ZENOPAY_BASE_URL}${endpoint}`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "x-api-key": process.env.ZENOPAY_API_KEY!,
+      "x-api-key": process.env.ZENOPAY_API_KEY || "API Not Found",
     },
     body: JSON.stringify(body),
   });
