c44a5a12f69f1ed8bc440b622a0d2f3382358b8f
Apple <bmcontractorstz@gmail.com>
2026-05-30 01:19:39 +0300

    overview supplier

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -164,7 +164,7 @@ export async function getProductBySlug(slug: string) {
 // Get single product by it's ID
 export async function getProductById(productId: string) {
   const data = await prisma.product.findFirst({
-    where: { id: productId },
+    where: { slug: productId },
     include: {
       pricingTiers: true,
     },
